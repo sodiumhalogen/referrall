68b04730d53108cd98cb8ed1ed7c9a90322fb99a
Chance Smith <chancesmithb@gmail.com>
2019-08-01 09:52:22 -0500

    📦  NEW: ditch andriod permission example

--- ReferAll/App.js
+++ ReferAll/App.js
@@ -20,21 +20,6 @@ class App extends PureComponent {
           style={styles.preview}
           type={RNCamera.Constants.Type.back}
           flashMode={RNCamera.Constants.FlashMode.on}
-          androidCameraPermissionOptions={{
-            title: 'Permission to use camera',
-            message: 'We need your permission to use your camera',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          androidRecordAudioPermissionOptions={{
-            title: 'Permission to use audio recording',
-            message: 'We need your permission to use your audio',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          onGoogleVisionBarcodesDetected={({ barcodes }) => {
-            console.log(barcodes);
-          }}
         />
         <View
           style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}
